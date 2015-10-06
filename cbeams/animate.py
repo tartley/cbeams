f0bc2371998a08f792c598b1609de780eed739c1
Jonathan Hartley <tartley@tartley.com>
2015-10-06 11:44:58 +0100

    get rid of Entity class for now, feels premature

--- cbeams/animate.py
+++ cbeams/animate.py
@@ -8,21 +8,21 @@ terminal = Terminal()
 
 class Shape():
 
-    def __init__(self, color, slivers):
-        self.color = color
+    def __init__(self, slivers):
         self.slivers = slivers
 
     def __str__(self):
-        return self.color + ''.join(
+        return ''.join(
             terminal.move(y, x) + ' ' * length
             for y, x, length in self.slivers
         )
 
-def rectfill(y1, x1, y2, x2):
-    return [
-        (y, x1, x2 - x1)
-        for y in range(y1, y2)
-    ]
+    @staticmethod
+    def rectfill(y1, x1, y2, x2):
+        return Shape([
+            (y, x1, x2 - x1)
+            for y in range(y1, y2)
+        ])
 
 
 def reset_on_exit(func):
@@ -37,28 +37,26 @@ def reset_on_exit(func):
 
 @reset_on_exit
 def animate():
-    for shape in [
-        Shape(terminal.on_blue, rectfill(1, 1, 2, 2)),
-        Shape(
+    for color, shape in [
+        (terminal.on_blue, Shape.rectfill(1, 1, 2, 2)),
+        (
             terminal.on_magenta,
-            rectfill(
-                terminal.height - 3, 1, terminal.height - 1, 2
-            )
+            Shape.rectfill(terminal.height - 3, 1, terminal.height - 1, 2)
         ),
-        Shape(
+        (
             terminal.on_green,
-            rectfill(
+            Shape.rectfill(
                 1, terminal.width - 3, 2, terminal.width - 1,
             )
         ),
-        Shape(
+        (
             terminal.on_red,
-            rectfill(
+            Shape.rectfill(
                 terminal.height - 3, terminal.width - 3,
                 terminal.height - 1, terminal.width - 1,
             )
         ),
     ]:
-        print(str(shape), sep='', end='')
+        print(color, str(shape), sep='', end='')
         sys.stdout.flush()
 
