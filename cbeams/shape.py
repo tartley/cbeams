d6a5bb440d58943a3ecd1b07d31786eeb2b499b4
Jonathan Hartley <tartley@tartley.com>
2016-01-05 20:36:36 -0600

    use 'strips' consistently instead of 'slices'

--- cbeams/shape.py
+++ cbeams/shape.py
@@ -3,7 +3,7 @@ from math import sqrt, trunc
 def rect(y1, x1, y2, x2):
     '''
     Given rectangle corners top left (y1, x1) to botto right (y2, x2) (incl),
-    returns that rectangle as a series of horizontal slices.
+    returns that rectangle as a series of horizontal strips.
     e.g. rect(0, 1, 2, 3) gives:
     [
         (0, 1, 3),
@@ -29,24 +29,24 @@ def rect(y1, x1, y2, x2):
 def disc(y, x, radius):
     '''
     Given a filled disc of int center (y, x) and float radius,
-    returns that disc as a series of horizontal slices.
+    returns that disc as a series of horizontal strips.
     Fractional parts of radius do make a visible difference.
     '''
     if radius < 0:
         raise ValueError('radius ({}) must be >0'.format(radius))
     elif radius == 0:
-        slices = []
+        strips = []
     else:
-        slices = []
+        strips = []
         for yoffset in range(-trunc(radius), trunc(radius) + 1):
             height = radius + yoffset
             xoffset = sqrt((radius - height / 2) * 2 * height)
-            slices.append((
+            strips.append((
                 y + yoffset,
                 x - trunc(xoffset),
                 trunc(xoffset) * 2 + 1
             ))
-    return Shape(slices)
+    return Shape(strips)
 
 class Shape():
     '''
