e8320f05827f93c1cb7009d837798e36df8b1d8c
Jonathan Hartley <tartley@tartley.com>
2016-01-10 15:22:53 -0600

    some lies

--- cbeams/animate.py
+++ cbeams/animate.py
@@ -7,24 +7,25 @@ from . import shape, terminal
 class Firework():
     '''
     A firework looks like an expanding annulus.
-    (i.e. a ring, a colored circle with a black hole in the middle).
+    (i.e. a colored circle with a hole in the middle).
     '''
     def __init__(self):
-        # Center point
+        # Center point.
         self.y, self.x = terminal.rand_coord()
 
         self.color = terminal.rand_color()
 
-        # The max radius we will reach
+        # The max radius we will reach.
         self.size = 2 + min(100, random.expovariate(0.2))
 
-        # How close 'outer' gets to size before inner hole appears
+        # How close 'outer' gets to its max size before 'inner' starts to grow.
+        # From 0 to 1
         self.thickness = random.triangular(0.2, 0.95)
 
-        # Outer and inner radius
+        # Outer and inner radii.
         self.outer = 0.0
         self.inner = 0.0
-        # The value of 'inner' on the last frame
+        # The value of 'inner' on the last frame.
         self.inner_last = self.inner
 
     def update(self):
@@ -39,10 +40,13 @@ class Firework():
         return self.inner > self.outer
 
     def draw(self):
+        # Draw the full coloured annulus from inner to outer radii.
         sys.stdout.write(self.color)
         sys.stdout.write(terminal.render(
             shape.annulus(self.y, self.x, self.outer, self.inner)
         ))
+        # Draw a very thin background colored annulus just inside 'inner',
+        # to erase the innermost part of the last frame's annulus.
         sys.stdout.write(terminal.terminal.on_color(0))
         sys.stdout.write(terminal.render(
             shape.annulus(self.y, self.x, self.inner, self.inner_last)
